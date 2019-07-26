882645eff9355f29e4b6ba7875730c465e6e8242
guojin <1075565141@qq.com>
2019-07-26 20:07:42 +0800

    提交

---
+++ src/com/itheima/test/FirstClass.java
@@ -0,0 +1,13 @@
+package com.itheima.test;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello,you are beautiful");
+        System.out.println("hello,you are beautiful");
+        System.out.println("hello,you are beautiful");
+        System.out.println("hello,you are beautiful");
+        System.out.println("hello,you are beautiful");
+        System.out.println("hello,you are beautiful");
+        System.out.println("hello,you are beautiful");
+    }
+}
